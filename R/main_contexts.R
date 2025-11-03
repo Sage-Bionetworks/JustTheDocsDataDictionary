dc10c9373cf96572c4b560aa6153881b58fdf255
jmvera255 <56414911+jmvera255@users.noreply.github.com>
2025-11-03 11:06:02 -0600

    incremental dev, have makeTemplateContent run on each context model and use xlsx or csv templates for accurate template representation

--- R/main_contexts.R
+++ R/main_contexts.R
@@ -1,26 +1,31 @@
 #' Main
 #' @description A function that executes the whole workflow of creating/updating gh-pages content from a data model.
-#' @param data_model_url a string indicating the https://raw.githubusercontent.com URL of the data model csv to be used.
+#' @param portal a string indicating the abbreviation of the portal: 'ark', 'veoibd'
+#' @param template_dir a string specifying the subdir where template files are stored, default = "model_templates"
+#' @param template_list a header-less txt file listing out all of the templates defined by the model
+#' @param branch a OPTIONAL string indicating subdir to which the model main branch has been downloaded to, default = "./"
 #' @return NULL
 #' @importFrom rlang .data
 #' @export
 
-main_contexts <- function(repo_raw_url) {
+main_contexts <- function(portal, template_dir = "model_templates", template_list, branch = "./") {
   # config repo space
   configure_space()
-  
+
   # download latest version of all_attributes.csv
-  url <- glue::glue("{repo_raw_url}/ark.all_attributes.csv")
-  all <- read.csv(url)
+  all <- read.csv(all_attr_url)
   # create/update metadata attribute content
   makeAttributeContent(all)
 
-  # remove mock templates
-  model <- dplyr::filter(model, !grepl("mock|test ", .data$Attribute, ignore.case = TRUE))
-
-  ## archive content for attributes no longer in the model
-  archive_content(model)
-
   # create/update metadata collection template content
-  makeTemplateContent(model)
+  fids <- list.files(file.path(branch, "model_contexts"), full.names = TRUE, recursive = TRUE)
+  fids <- fids[grepl("model\\.csv$", fids)]
+  models <- purrr::map(fids, read.csv)
+  # process each context model separately so that each template page renders context-specific details
+  purrr::walk(models, function(model, template_dir_path) {
+    makeTemplateContent(model, template_dir)
+  }, template_dir_path = template_dir)
+
+  ## archive content that is not longer defined in the data model
+  archive_content("???")
 }
